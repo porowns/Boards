7c1333e04a548c0e8a0f5e22b485cf9f83a1f42b
porowns <porowns@gmail.com>
2017-05-12 17:53:32 -0400

    Adding urls, updating views with initital index.html

--- Boards/Boards/settings.py
+++ Boards/Boards/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'Boards.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./Boards/templates', './app/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
