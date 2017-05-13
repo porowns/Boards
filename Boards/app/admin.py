7c7dde68a3d47ff278992516432960846a5bfe97
porowns <porowns@gmail.com>
2017-05-13 19:04:57 -0400

    Added Post and Board interaction in admin.py

--- Boards/app/admin.py
+++ Boards/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post, Board
 
 # Register your models here.
+@admin.register(Post)
+class Post(admin.ModelAdmin):
+    list_display = ('title', 'body', 'author')
+@admin.register(Board)
+class Board(admin.ModelAdmin):
+    list_display = ('title', 'category', 'moderator')
